c78d13586bc423b6f5314205d57db683b903af81
Poossppaa <luk.pospisil@gmail.com>
2019-11-19 17:13:19 +0100

    added drawables and basic game logic

--- app/src/main/java/com/example/tamz2project/MainActivity.java
+++ app/src/main/java/com/example/tamz2project/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.tamz2project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new GameView(this));
+
     }
 }
+
